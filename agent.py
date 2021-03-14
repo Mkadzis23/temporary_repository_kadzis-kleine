ff52a920c5717c16d02d48d2f139ee05fb12a974
jdk31 <jdk31@students.uwf.edu>
2021-03-14 00:27:54 -0600

    Update agent.py

--- agent.py
+++ agent.py
@@ -3,9 +3,27 @@ import sys
 import numpy as np
 from data.map import Map
 import gameState as gs
+from data.node import Node
+def Node(gameMap): 
+
 
     #takes in the current board state and returns a slot that is the move choice
-def alphBetSearch(gameMap):\
+def alphBetSearch(gameMap):
+    alpha = -999999999
+    beta = 999999999
+    a = None
+    node =  Node(gameMap)
+    for slot in range(node.map_size):
+        if(gs.checkSlot(slot)):
+            alphaPrime = minValue(node, 
+            #make tree for that slot
+        else:
+            #Reached leaf, so call utility func
+             return evaluateStateValue(gameMap)
+    def minValue(node, problem, alpha, beta)
+
+
+
     #Generate entire game tree up to a certain depth
     #apply utility function to the leaves
     #Propagate upwards to the root (max nodes receiving the max of their children and the opposite for min nodes)
@@ -19,4 +37,4 @@ def evaluateStateValue(gameMap):
     # Add a value to the state's value based on how many of the above options there are in a row
     # (Possibly 1pt for 2, 2pts for 3, etc.) And subtract the same value for the opponent 
     # return the net value
-    return
+    return   
