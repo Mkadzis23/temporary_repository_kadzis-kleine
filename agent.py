64c344ffdb6f90557741925d58aff575c0e05c33
jdk31 <jdk31@students.uwf.edu>
2021-03-15 22:45:17 -0500

    Simplified evaluatestatevalue greatly

--- agent.py
+++ agent.py
@@ -58,16 +58,25 @@ def minMaxSearch (gameMap, depth, isMax, m, w, s):
         return best_score
         
 
-#takes in a board state and returns a value that represents how favorable it is. 
+    #returns the net value of a game state
 def evaluateStateValue(gameMap, win_length, matrix_size, currPlayerSymbol):
+    otherSymbol = 0
+    if currPlayerSymbol == 0:
+        otherSymbol = 1
+    return evaluate(gameMap, win_length, matrix_size, currPlayerSymbol) - evaluate(gameMap, win_length, matrix_size, otherSymbol)
+
+#takes in a board state and returns a value that represents how favorable it is. 
+def evaluate(gameMap, win_length, matrix_size, currPlayerSymbol):
     # Could be just like the gameOver function but look for 2, 3, up to win_length - 1 in a row (with an empty slot available still) <- doesnt do that last part yet
     # Add a value to the state's value based on how many of the above options there are in a row
     # (Possibly 1pt for 2, 2pts for 3, etc.) And subtract the same value for the opponent 
     # return the net value
     stateScore = 0
     inaRow = 0
-
-    #####FOR CURRENT PLAYER
+    #setting the opponent's Symbol
+    opponentSymbol = 0
+    if currPlayerSymbol == 0:
+        opponentSymbol = 1
 
     #check for 0 row victory
     for row in gameMap.map_state:
@@ -75,7 +84,7 @@ def evaluateStateValue(gameMap, win_length, matrix_size, currPlayerSymbol):
         for column in row:
             if column == currPlayerSymbol:
                 inaRow += 1
-                if inaRow > 1:
+                if inaRow >= 1:
                     #only need to add 1 each time, since this will happen for each slot counted as being in a row
                     stateScore += 1
                 if inaRow == win_length: return 1
@@ -119,63 +128,6 @@ def evaluateStateValue(gameMap, win_length, matrix_size, currPlayerSymbol):
                     negative_diagonal_count = negative_diagonal_count + 1
                     if negative_diagonal_count > 1:
                         stateScore += 1
-   
-   
-    #####FOR 'ENEMY' PLAYER
-    if currPlayerSymbol == 0:
-        currPlayerSymbol = 1
-    else:
-        currPlayerSymbol = 0
-    for row in gameMap.map_state:
-        inaRow = 0
-        for column in row:
-            if column == currPlayerSymbol:
-                inaRow += 1
-                if inaRow > 1:
-                    #only need to subtract 1 each time, since this will happen for each slot counted as being in a row
-                    stateScore -= 1
-                if inaRow == win_length: return 1
-            else:
-                inaRow = 0
-
-    #check 0 column victory
-    for x in range(matrix_size):
-        inaRow = 0
-        for column in gameMap.map_state:
-            if column[x] == currPlayerSymbol:
-                inaRow += 1
-                if inaRow > 1:
-                    #only need to subtract 1 each time, since this will happen for each slot counted as being in a row
-                    stateScore -= 1
-                if inaRow == win_length: return 1
-            else:
-                inaRow = 0
-
-    #Testloop for diagonal left to right
-    index_diff = matrix_size - win_length + 1
-    map_size = matrix_size - 1
-
-    for i in range(index_diff):
-        for j in range(index_diff):
-            positive_diagonal_symbol = gameMap.map_state[map_size - i][j]
-            positive_diagonal_count = 0
-
-            negative_diagonal_symbol = gameMap.map_state[i][j]
-            negative_diagonal_count = 0
-
-            for count in range(win_length):
-                current_positive_symbol = gameMap.map_state[map_size - i - count][j + count]
-                if current_positive_symbol == positive_diagonal_symbol:
-                    positive_diagonal_count = positive_diagonal_count + 1
-                    if positive_diagonal_count > 1:
-                        stateScore -= 1
-
-                current_negative_symbol = gameMap.map_state[i + count][j + count]
-                if current_negative_symbol == negative_diagonal_symbol:
-                    negative_diagonal_count = negative_diagonal_count + 1
-                    if negative_diagonal_count > 1:
-                        stateScore -= 1
-    #print(stateScore)
     return   stateScore
 
 
